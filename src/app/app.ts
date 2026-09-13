9c21458a7454bff5845e499915fd3ed9bc4fcadb
ketlinvgp1 <ketlinvgp@gmail.com>
2026-09-12 22:54:56 -0300

    Tchauzinho

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,7 @@
 import { Component, signal } from '@angular/core';
-<<<<<<< HEAD
+
 import { RouterOutlet,RouterLink } from '@angular/router';
-=======
-import { RouterLink, RouterOutlet } from '@angular/router';
->>>>>>> 7324f583a7e33014c218b565437a7f6aa3d18674
+
 
 @Component({
   imports: [RouterOutlet, RouterLink],
